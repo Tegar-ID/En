d9028e2852b32980f6cdfe75ec52f7549c874307
Tegar-ID <69099460+Tegar-ID@users.noreply.github.com>
2020-08-06 19:31:58 +0700

    Add files via upload

---
+++ tampilan.py
@@ -0,0 +1,44 @@
+#!usr/bin/python2.7
+# -*- coding: UTF-8 -*-
+
+#Module
+import os
+#kode warna
+wd = "\033[90;1m" # dark
+GL = "\033[96;1m" # Blue aqua
+BB = "\033[34;1m" # Blue light
+YY = "\033[33;1m" # Yellow light
+GG = "\033[32;1m" # Green light
+WW = "\033[0;1m"  # White light
+RR = "\033[31;1m" # Red light
+CC = "\033[36;1m" # Cyan light
+B = "\033[34m"    # Blue
+Y = "\033[33;1m"    # Yellow
+G = "\033[32m"    # Green
+W = "\033[0;1m"     # White
+R = "\033[31m"    # Red
+C = "\033[36;1m"    # Cyan
+#tampilan
+os.system("clear")
+
+print C+"  ____________"
+print C+"  ║"+G+"▒▒▒▒▒▒▒▒▒▒"+C+"║"
+print C+"  ║"+G+"▒▒▒▒▒▒▒▒▒▒"+C+"║           "+B+"╔══╗╔═╗╔══╗╔══╗╔═╗  "+R+"╔══╗╔══╗"
+print C+"  ║"+G+"▒▒▒▒▒▒▒▒▒▒"+C+"║           "+B+"╚╗╔╝║╦╝║╔═╣║╔╗║║╬║  "+R+"╚║║╝╚╗╗║"
+print C+"  ║"+G+"▒▒▒▒▒▒▒▒▒▒"+C+"║           "+B+"─║║─║╩╗║╚╗║║╠╣║║╗╣  "+R+"╔║║╗╔╩╝║"
+print C+"  ║"+G+"▒▒▒▒▒▒▒▒▒▒"+C+"║           "+B+"─╚╝─╚═╝╚══╝╚╝╚╝╚╩╝  "+R+"╚══╝╚══╝"
+print C+"  ║"+G+"▒▒▒▒▒▒▒▒▒▒"+C+"║"
+print C+" ╔════════════╗        "+Y+"["+R+"!"+Y+"] "+W+"Compile file by obfuscate "+Y+"["+R+"!"+Y+"]"
+print C+" ╚════════════╝"
+print C+"  ║"+Y+"██████████"+C+"╚╗            "+R+"WhatsApp "+Y+": "+G+"082125068665"
+print C+"  ║"+Y+"██"+C+"╔══╗"+Y+"█"+C+"╔═╗"+Y+"█"+C+"║            "+R+"Facebook "+Y+": "+G+"Tegar Sabila"
+print C+"  ║"+Y+"██"+C+"║"+R+"╬"+C+"╔╝"+Y+"█"+C+"╚╗"+C+"║"+Y+"█"+C+"║"
+print C+"  ║"+Y+"██"+C+"╚═╝"+Y+"█"+C+"║"+Y+"█"+C+"╚╝"+Y+"█"+C+"║          "+Y+"["+R+"RG4"+Y+"] "+G+"Black_Coder   "+R+"┏━┳┳┳━┳┳┓"
+print C+"  ╚╗"+Y+"█████████"+C+"═╝           "+G+"+"+C+"./P3R50N G4N5     "+R+"┃━┫┃┃┏┫━┫┏┓"
+print C+"    ╚╗║"+W+"╠╩╩╩╩╩╝            "+G+"+"+C+"Mr XHamster       "+R+"┃┏┫┃┃┗┫┃┃┃┃"
+print C+"     ║║┈┈┈█"+YY+"▐"+WW+"█████"+R+"▒"+W+".｡oO    "+G+"+"+C+"MeYouSue          "+R+"┗┛┗━┻━┻┻┛┃┃"
+print C+"     ║"+Y+"██"+W+"╠╦╦╦╗                                "+W+"┏┳┳━┳┳┳┓┏┫┣┳┓"
+print C+"     ╚╗"+Y+"██████                                "+W+"┃┃┃┃┃┃┃┃┣┻┫┃┃"
+print C+"      ╚════╝                                 "+W+"┣┓┃┃┃┃┣┫┃┏┻┻┫"
+print C+"                                             "+W+"┗━┻━┻━┻"+C+"❢"
+print " "
